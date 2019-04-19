08178591b91fda1a6d3782adea22cbf79ab12d59
wipegup <wipegup@gmail.com>
2019-04-19 10:15:28 -0600

    Jungle beats tests passing

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -7,6 +7,21 @@ class JungleBeat
   end
 
   def append(string)
-    
+    start = 0
+    space = string.index(" ")
+    while space != nil
+      @list.append(string[start..(space-1)])
+      if space + 1 > string.length
+        break
+      end
+      start = space +1
+      space = string.index(" ", start)
+    end
+    @list.append(string[start..-1])
+    return string
+  end
+
+  def count
+    @list.count
   end
 end
--- test/jungle_beats_test.rb
+++ test/jungle_beats_test.rb
@@ -1,19 +1,18 @@
 require './test/test_helper'
 require './lib/jungle_beat'
 
-class LinkedListTest < MiniTest::Test
+class JungleBeatTest < MiniTest::Test
   def setup
     @jb = JungleBeat.new
   end
 
   def test_initialized_with_empty_list
-    assert_instance_of LinkedList, @jb.list.class
+    assert_instance_of LinkedList, @jb.list
     assert_nil @jb.list.head
   end
 
   def test_it_can_add_multiple_at_one_time
     @jb.append("this it that")
-    binding.pry
 
     assert_equal 3, @jb.count
     assert_equal "this", @jb.list.head.data
